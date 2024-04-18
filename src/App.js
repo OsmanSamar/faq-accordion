7f57194c99c908df04b13d3a6de9e457eff05b7e
Samar <osmansamar367@gmail.com>
2024-04-18 08:56:43 +0200

    mt updated

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
         boxShadow="xl" // Box shadow
         p="21px" // Padding
         // mt={{ base: "115px", md: "150px", lg: "220px" }} // Responsive margin-top
-        mt="150px"
+        mt="170px"
         pos="relative" // Position
         mx="auto" // Margin horizontal
       >
